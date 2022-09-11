dbdc5dd5afc321d5d61670823ef88d0e0634b613
LorenaPinkala <lorenapinkala77@gmail.com>
2022-09-10 21:11:54 -0300

    navbar con su barra de busqueda

---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+//import Badge from '@mui/material/Badge';
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import { styled } from "@mui/material";
+import Searchbar from "./Searchbar";
+
+const StyledToolBar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  backgroundColor: "#7f00b2",
+  justifyContent: "space-between",
+  borderRadius: theme.shape.borderRadius,
+}));
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <StyledToolBar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            MangaLove
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton size="large" aria-label="" color="inherit"></IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Searchbar />
+              {/* <Badge badgeContent={17} color="error">
+              <NotificationsIcon />
+            </Badge> */}
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              // aria-controls={menuId}
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              // aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </StyledToolBar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
