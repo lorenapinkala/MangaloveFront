50f679d375a2ec1146a0c0d2e0cc98b4042adc89
LorenaPinkala <lorenapinkala77@gmail.com>
2022-09-13 20:28:16 -0300

    ruteo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import { Box, CssBaseline } from "@mui/material";
-// import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
@@ -8,12 +8,12 @@ function App() {
       <CssBaseline />
       <Box>
         <Navbar />
-        {/* <Routes>
+        <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/signup" element={<Signup/>} />
+          {/* <Route path="/signup" element={<Signup/>} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/profile" element={<Profile/>} />
-        </Routes> */}
+          <Route path="/profile" element={<Profile/>} /> */}
+        </Routes>
       </Box>
     </>
   );
