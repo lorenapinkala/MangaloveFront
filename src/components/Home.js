765ff1c8027054b5172b68ed24932ceb8961c7cb
LorenaPinkala <lorenapinkala77@gmail.com>
2022-09-13 20:09:51 -0300

    componente home y creacion de los archivos que lo componen

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import MangasGrill from './MangasGrill';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Header/>
+      <MangasGrill />
+    </>
+  )
+}
+
+export default Home
